6631240ab39ac576bf85df22763f1ca5808dd063
mahi <mudrabaoinamahi@gmail.com>
2023-09-09 22:59:26 +0530

    java file updated

--- Hello-world.java
+++ Hello-world.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java and welcome to new world");  
     }  
 }   
 
